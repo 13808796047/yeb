93b3d6a31fe6e8fb0dc1f694f155f18d42122fbd
Summer <511391805@qq.com>
2022-03-04 15:59:24 +0800

    :sparkles:公共返回对象

---
+++ yeb-server/src/main/java/com/example/server/config/security/RestAuthorizationEntryPoint.java
@@ -0,0 +1,32 @@
+package com.example.server.config.security;
+
+import com.example.server.pojo.RespBean;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * 当登录或者Token失效时访问接口时，自定义返回结果
+ */
+@Component
+public class RestAuthorizationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        httpServletResponse.setCharacterEncoding("UTF-8");
+        httpServletResponse.setContentType("application/json");
+        PrintWriter out = httpServletResponse.getWriter();
+        RespBean bean = RespBean.error("权限不足，请联系管理员");
+        bean.setCode(401);
+        out.write(new ObjectMapper().writeValueAsString(bean));
+        out.flush();
+        out.close();
+
+    }
+}
--- yeb-server/src/main/java/com/example/server/service/IAdminService.java
+++ yeb-server/src/main/java/com/example/server/service/IAdminService.java
@@ -2,15 +2,34 @@ package com.example.server.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.example.server.pojo.Admin;
+import com.example.server.pojo.RespBean;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * <p>
- *  服务类
+ * 服务类
  * </p>
  *
  * @author zhanglishen
  * @since 2022-03-04
  */
 public interface IAdminService extends IService<Admin> {
+    /**
+     * 登录之后返回Token
+     *
+     * @param username
+     * @param password
+     * @param request
+     * @return
+     */
+    RespBean login(String username, String password, HttpServletRequest request);
 
+    /**
+     * 根据用户获取用户
+     *
+     * @param username
+     * @return
+     */
+    Admin getAdminByUserName(String username);
 }
